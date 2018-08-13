5ab755809e92eeb28a16f3976a38da6b11cb612c
Josh Lapham <josh@joshlapham.com>
2018-08-13 15:46:15 +1000

    Continue implementing React Native FlatList component

--- index.js
+++ index.js
@@ -7,25 +7,15 @@ class RNWeatherResultsList extends React.Component {
       <View style={styles.container}>
         <FlatList
           data={this.props['consolidated_weather']}
-          renderItem={({item}) => <Text style={styles.item}>{item.max_temp}</Text>}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) =>
+            <Text style={styles.item}>
+              Temperature: {item.the_temp}{'\n'}
+              Condition: {item.weather_state_name}{'\n'}
+              Time: {item.created}
+            </Text>
+          }
         />
-      </View>
-    );
-  }
-}
-
-class RNHighScores extends React.Component {
-  render() {
-    var contents = this.props['consolidated_weather'].map((data) => (
-      <Text key={data.max_temp}>
-        {data.max_temp}:{data.value}
-        {'\n'}
-      </Text>
-    ));
-    return (
-      <View style={styles.container}>
-        <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
-        <Text style={styles.scores}>{contents}</Text>
       </View>
     );
   }
@@ -34,24 +24,12 @@ class RNHighScores extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-  },
-  highScoresTitle: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  scores: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    paddingTop: 22,
   },
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
+    height: 88,
   },
 });
 
